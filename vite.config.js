d6c2c318c2b5b75e79118426de6451aa2a193f8e
Chirag <61862973+chirag-dewan@users.noreply.github.com>
2025-02-17 14:13:13 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   resolve: {
     alias: {
       "react-router-dom": fileURLToPath(new URL("./node_modules/react-router-dom", import.meta.url)),
-      "react-github-calendar": fileURLToPath(new URL("./node_modules/react-github-calendar", import.meta.url))
+      "react-github-calendar": fileURLToPath(new URL("./node_modules/react-github-calendar/dist/index.js", import.meta.url))
     }
   }
 });
