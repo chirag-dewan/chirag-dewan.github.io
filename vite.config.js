5fbcbee6a91b902668e01edd48dff9407aedb1ea
Chirag <61862973+chirag-dewan@users.noreply.github.com>
2025-02-17 14:18:18 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
   plugins: [react()],
+  // No 'base' for a user/organization GH Pages site like chirag-dewan.github.io
   optimizeDeps: {
+    // Not strictly required, but can help ensure these are pre-bundled
     include: ["react-router-dom", "react-github-calendar"]
   },
-  resolve: {
-    alias: {
-      "react-router-dom": fileURLToPath(new URL("./node_modules/react-router-dom", import.meta.url)),
-      "react-github-calendar": fileURLToPath(new URL("./node_modules/react-github-calendar/dist/index.js", import.meta.url))
-    }
-  }
 });
