47d9dfc5b804cdd15f6be1d3574087a4d9adf478
Chirag <61862973+chirag-dewan@users.noreply.github.com>
2025-03-08 16:04:20 -0600

    Create Contact.jsx

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ContactComponent from '../components/home/Contact';
+
+const Contact = () => {
+  return (
+    <div className="pt-28 pb-20">
+      <ContactComponent />
+    </div>
+  );
+};
+
+export default Contact;
